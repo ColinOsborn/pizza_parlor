a5c59dd6d10ac321a019693e2e1796f1c9d362f0
Colin Osborn <colin.osborn@gmail.com>
2016-03-01 10:53:49 -0700

    making tests

--- test/topping_test.rb
+++ test/topping_test.rb
@@ -8,4 +8,10 @@ class ToppingTests < Minitest::Test
   def test_setup
     assert_respond_to
   end
+
+  def test_it_has_a_name
+    names = Toppings.new()
+
+    assert_equal " ", names.contents
+  end
 end
