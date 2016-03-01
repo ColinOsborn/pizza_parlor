5b77109c5c0fd860b3090737488933813ead29d4
Colin Osborn <colin.osborn@gmail.com>
2016-03-01 13:49:26 -0700

    changes

--- lib/topping.rb
+++ lib/topping.rb
@@ -4,23 +4,23 @@ require 'csv'
 
 class Topping
   attr_reader :name,
-              :price,
-              :is_vegetarian
+  :price,
+  :is_vegetarian
 
-    def initialize(name, price, is_vegetarian)
-      @name = name
-      @price = price
-      @is_vegetarian = is_vegetarian
+  def initialize(name, price, is_vegetarian)
+    @name = name
+    @price = price
+    @is_vegetarian = is_vegetarian
+  end
+
+  def read_csv(location)
+    contents = CSV.open file_path, headers: true, header_converters: true
+    contents = csv_table.each do |row|
+      name = row[:name]
+      price = row[:price]
+      is_vegetarian = row[:is_vegetarian]
+      menu << Topping.new(name, price, is_vegetarian)
     end
-
-    def read_csv(location)
-      contents = CSV.open file_path, headers: true, header_converters: true
-      contents = csv_table.each do |row|
-        name = row[:name]
-        price = row[:price]
-        is_vegetarian = row[:is_vegetarian]
-        menu << Topping.new(name, price, is_vegetarian)
-      end
   end
 
 
